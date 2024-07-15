e97499191f6bb811d3d925c34e5133bd67bc9a59
Subha Maity <maitysubha1321@gmail.com>
2024-07-15 11:48:29 +0530

    done day_1

---
+++ Day_1/datatypes.js
@@ -0,0 +1,40 @@
+// Activity 1
+
+var num = 45
+console.log(num);
+let cricketer = "Rohit Sharma"
+console.log(cricketer);
+
+// Activity 2
+
+const isOpen = true
+console.log(isOpen);
+
+// Activity 3
+
+const numOfApples = 23
+const favGadget ="Macbook Air 15"
+const isLoggedIn = false
+const obj ={
+    name:"Subha",
+    language:"JS"
+}
+const arr =["mouse","monitor","cpu"]
+
+console.table([typeof numOfApples,typeof favGadget,typeof isLoggedIn,typeof obj,typeof arr])
+
+
+// Activity 4
+let fruit ="banana"
+console.log(fruit);//banana
+
+ fruit ="apple"
+console.log(fruit); //apple
+
+// Activity 5
+
+const flower ="lotus"
+console.log(flower);
+ 
+// flower = "rose"
+console.log(flower); //showing typeerror 
